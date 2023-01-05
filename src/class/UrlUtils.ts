08ba52243cb94b127de0a6db56fdd02e3a10ba5f
guilhermealvesc <guilherme.092011@hotmail.com>
2023-01-05 13:42:08 -0300

    refat: UrlUtils isWikiAndDateless componentization

--- src/class/UrlUtils.ts
+++ src/class/UrlUtils.ts
@@ -1,13 +1,22 @@
 export class UrlUtils {
 	static isWikiAndDateless(url: string): boolean {
-		if (
-			url?.startsWith('/wiki') &&
-			isNaN(Number(url?.split('/')[2].split('_')[0]))
-		) {
+		if (url?.startsWith('/wiki') && this.hasDateOnUrlRoute(url)) {
 			return true;
 		}
 
 		return false;
+	}
+
+	private static hasDateOnUrlRoute(url: string): boolean {
+		return isNaN(Number(this.getFirstUrlRouteSplittedBy(url, '_')));
+	}
+
+	private static getFirstUrlRouteSplittedBy(url: string, byToken: string) {
+		return this.getFirstUrlRoute(url).split(byToken);
+	}
+
+	private static getFirstUrlRoute(url: string) {
+		return url?.split('/')[2];
 	}
 
 	static getFullUrl(base: string, route: string) {
